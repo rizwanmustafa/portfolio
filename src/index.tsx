8bdb6f2864dcf72eea28d9449e7deee02bdd183e
Rizwan Mustafa <rizwanmustafa0000@gmail.com>
2022-02-13 21:12:40 +0500

    Added firebase deployment

--- src/index.tsx
+++ src/index.tsx
@@ -10,6 +10,27 @@ import NavBar from "./components/NavBar/NavBar";
 
 import "./index.css";
 
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+// Initialize Firebase
+const firebaseConfig = {
+	appId: process.env.REACT_APP_APP_ID,
+	apiKey: process.env.REACT_APP_API_KEY,
+	authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+	projectId: process.env.REACT_APP_PROJECT_ID,
+	storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+	messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
+	measurementID: process.env.REACT_APP_MEASUREMENT_ID
+}
+const firebaseApp = initializeApp(firebaseConfig);
+const analytics = getAnalytics(firebaseApp);
+
 // Lazy load non-essential components
 const App = lazy(() => import("./App"));
 const ProjectList = lazy(() => import("./components/ProjectList/ProjectList"));
