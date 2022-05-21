dceef00cbeb14574e1794afa25d88a00266b9dc4
Rizwan Mustafa <rizwanmustafa0000@gmail.com>
2022-05-21 15:18:49 +0500

    remove site under construction notice

--- src/App.tsx
+++ src/App.tsx
@@ -8,17 +8,10 @@ const Introduction = lazy(() => import("./components/Introduction/Introduction")
 const ProjectList = lazy(() => import("./components/ProjectList/ProjectList"));
 
 
-const SiteUnderConstruction = (): JSX.Element => {
-	return (
-		<p style={{ backgroundColor: "red", margin: 0, padding: "0.5rem", color: "white", textAlign: "center" }}>Note: This website is under construction!</p>
-	);
-}
-
 const App = (): JSX.Element => {
 	return (
 		<ThemeProvider theme={theme}>
 			<Suspense fallback={<Loading />} >
-				<SiteUnderConstruction />
 				<Introduction />
 				<ProjectList />
 			</Suspense>
